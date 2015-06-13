6032536dad810f70245b54ce8af64692ca7f5ac0
Izou <contact@izzdesign.fr>
2015-06-13 19:29:20 +0200

    buttons + video + subtitles

--- js/steps.js
+++ js/steps.js
@@ -1,7 +1,16 @@
 $(function() {
+  
   ong1 = document.getElementById('a1');
   ong2 = document.getElementById('a2');
   ong3 = document.getElementById('a3');
+  ong4 = document.getElementById('a4');
+  ong5 = document.getElementById('a5');
+  ong6 = document.getElementById('a6');
+  ong7 = document.getElementById('a7');
+  ong8 = document.getElementById('a8');
+  ong9 = document.getElementById('a9');
+  ong10 = document.getElementById('a10');
+  ong11 = document.getElementById('a11');
   var video = document.getElementById('video-tab');
   var src = video.getAttribute('src');
 
@@ -15,6 +24,14 @@ $(function() {
     ong1.className = "";
     ong2.className = "";
     ong3.className = "";
+    ong4.className = "";
+    ong5.className = "";
+    ong6.className = "";
+    ong7.className = "";
+    ong8.className = "";
+    ong9.className = "";
+    ong10.className = "";
+    ong11.className = "";
   }
 
   function active(moi){
@@ -24,17 +41,57 @@ $(function() {
   }
 
   ong1.addEventListener("click", function() {
-    video.setAttribute('src', '../video/01.wikipedia.mp4');
+    video.setAttribute('src', './video/01.wikipedia_2.mp4');
     active(this);
   });
 
   ong2.addEventListener("click", function() {
-    video.setAttribute('src', '../video/explications.mp4');
+    video.setAttribute('src', './video/02.thomasheine.mp4');
     active(this);
   });
 
   ong3.addEventListener("click", function() {
-    video.setAttribute('src', '../video/explications.mp4');
+    video.setAttribute('src', './video/explications.mp4');
+    active(this);
+  });
+
+    ong4.addEventListener("click", function() {
+    video.setAttribute('src', './video/04.simplicissimus.mp4');
+    active(this);
+  });
+
+    ong5.addEventListener("click", function() {
+    video.setAttribute('src', './video/explications.mp4');
+    active(this);
+  });
+
+    ong6.addEventListener("click", function() {
+    video.setAttribute('src', './video/explications.mp4');
+    active(this);
+  });
+
+    ong7.addEventListener("click", function() {
+    video.setAttribute('src', './video/01.wikipedia_2.mp4');
+    active(this);
+  });
+
+    ong8.addEventListener("click", function() {
+    video.setAttribute('src', './video/explications.mp4');
+    active(this);
+  });
+
+    ong9.addEventListener("click", function() {
+    video.setAttribute('src', './video/explications.mp4');
+    active(this);
+  });
+
+    ong10.addEventListener("click", function() {
+    video.setAttribute('src', './video/explications.mp4');
+    active(this);
+  });
+
+    ong11.addEventListener("click", function() {
+    video.setAttribute('src', './video/01.wikipedia_2.mp4');
     active(this);
   });
 });
